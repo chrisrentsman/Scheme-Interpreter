7010a4f7acbafd572c256ad5762713ae86c60b0d
Chris <chrisrentsman601@gmail.com>
2015-11-16 15:55:35 -0500

    Commented list.h

--- list.h
+++ list.h
@@ -1,17 +1,17 @@
-/****************************************************************
+/**********************************************************************
  File: list.h
  -------------
  Contains the interface the list structure. The list structure
  is one of the primary building blocks within Scheme. Lists
  can either point at two more lists, or contain a string.
- ****************************************************************/
+ *********************************************************************/
 
 #ifndef CELL
 #define CELL
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
+/**********************************************************************
  *  Data Type: List
  *  ---------------
  *  Declares a list in the following way:
@@ -21,7 +21,7 @@
 struct list;
 typedef struct list * List;
 
-/**
+/**********************************************************************
  *  Function: createList()
  *  ----------------------
  *  Creates a new List and returns it, as in:
@@ -30,10 +30,27 @@ typedef struct list * List;
  */
 List createList();
 
+/**********************************************************************
+ *  Functions: setFirst(List source, List sink), getFirst(List source)
+ *  ------------------------------------------------------------------
+ *  Sets and retrieves the first element of a list.
+ */
 void setFirst(List source, List sink);
 List getFirst(List source);
+
+/**********************************************************************
+ *  Functions: setRest(List source, List sink), getRest(List source)
+ *  ------------------------------------------------------------------
+ *  Sets and retrieves the rest of the list.
+ */
 void setRest(List source, List sink);
 List getRest(List source);
+
+/**********************************************************************
+ *  Functions: setSymbol(List source, char * symbol), getSymbol(List source)
+ *  ------------------------------------------------------------------
+ *  Sets and retrieves the symbol of a list.
+ */
 void setSymbol(List source, char * symbol);
 char * getSymbol(List source);
 
